6e5755c4b5cf2e9c1f19d53afde9f69a77f4a63a
Lucas <lucasimionato1910@hotmail.com>
2020-12-16 15:46:18 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ListaObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
