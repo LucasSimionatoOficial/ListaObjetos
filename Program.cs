10156e41a2e7bfa7c199e0f934723955d30651fd
Lucas <lucasimionato1910@hotmail.com>
2020-12-16 15:47:47 -0300

    Concluido

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using ListaObjetos.classes;
 
 namespace ListaObjetos
 {
@@ -6,7 +8,33 @@ namespace ListaObjetos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Produto> produtos = new List<Produto>();
+            produtos.Add(new Produto(1, "Apple Watch", 3522.56f));
+            produtos.Add(new Produto(2, "Xiaomi Watch", 1522.45f));
+            produtos.Add(new Produto(3, "Zenfone Asus", 2522.87f));
+            produtos.Add(new Produto(4, "Sansung", 2522.87f));
+            produtos.Add(new Produto(5, "Motorola G8", 2522.87f));
+
+            Produto iphone = new Produto();
+            iphone.Codigo = 6;
+            iphone.Nome = "Iphone XII";
+            iphone.Preco = 8952.45f;
+
+            produtos.Add(iphone);
+
+            foreach (Produto item in produtos){
+                Console.WriteLine($"{item.Nome} - R${item.Preco}");
+            }
+
+            produtos.RemoveAt(3);
+
+            produtos.RemoveAll(x => x.Nome == "Apple Watch");
+
+            Console.WriteLine("\nLista alterada");
+
+            foreach (Produto item in produtos){
+                Console.WriteLine($"{item.Nome} - R$ {item.Preco}");
+            }
         }
     }
 }
